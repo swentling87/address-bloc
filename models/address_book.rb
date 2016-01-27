e562ba0b33612cc9b91ba4425ce509bc7d42cfda
Stephen Wentling <swentling87@gmail.com>
2016-01-26 21:11:36 -0800

    checkpoint-5 completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -3,9 +3,11 @@ require "csv"
 
 class AddressBook
   attr_accessor :entries
+
   def initialize
     @entries = []
   end
+
   def add_entry(name, phone_number, email)
     index = 0
     @entries.each do |entry|
@@ -16,6 +18,7 @@ class AddressBook
     end
     @entries.insert(index, Entry.new(name, phone_number, email))
   end
+
   def remove_entry(name, phone_number, email)
     removed_entry = nil
     @entries.each do |entry|
@@ -25,7 +28,6 @@ class AddressBook
     end
     @entries.delete(removed_entry)
   end
-
   def import_from_csv(file_name)
     csv_text = File.read(file_name)
     csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
@@ -35,4 +37,27 @@ class AddressBook
       add_entry(row_hash["name"] , row_hash["phone_number"], row_hash["email"])
     end
   end
+
+  def binary_search(name)
+
+    lower = 0
+    upper = @entries.length - 1
+
+    while lower <= upper
+
+      mid = (lower + upper) / 2
+      mid_name = @entries[mid].name
+
+      if name == mid_name
+        return @entries[mid]
+      elsif name < mid_name
+        upper = mid -1
+      elsif name > mid_name
+        lower = mid + 1
+      end
+    end
+
+    return nil
+  end
+
 end
